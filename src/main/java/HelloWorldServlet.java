6a80bc7af51f841f7c03390ee4911e154c52f822
Joshua Rooks <joshua.e.rooks@gmail.com>
2023-01-26 14:09:26 -0600

    finished servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,13 +8,20 @@ import java.io.PrintWriter;
 
 @WebServlet(name="HelloWorld", urlPatterns ="/hello")
 public class HelloWorldServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         String name = req.getParameter("name");
+        if(name != null){
+            writer.println("<h1>Hello " + name + "!</h1>");
+        }else {
+            writer.println("<h1>Hello World!</h1>");
+        }
 
-        writer.println("<h1>Hello World!</h1>");
-        writer.println("<h1>Hello " + name + "!</h1>");
+
+
+
     }
 
 }
