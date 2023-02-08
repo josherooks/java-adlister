5b347635206d2aff60a1cc963b8b9f4b19f2cfee
Joshua Rooks <joshua.e.rooks@gmail.com>
2023-02-08 16:05:11 -0600

    double checked sessions/cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
         if(request.getSession().getAttribute("user") != null){
             response.sendRedirect("/profile");
             return;
@@ -22,8 +21,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
-            request.getSession().setAttribute("username");
-
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
